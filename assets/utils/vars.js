0ffa9bd46dea0b7492289961c1791eda756e6e3b
Paradoxe Ngwasi <paradoxengwasi@gmail.com>
2021-02-03 09:44:41 +0200

    refact: access var GOOGLE_API_KEY from env file

--- assets/utils/vars.js
+++ assets/utils/vars.js
@@ -4,4 +4,4 @@ export const openModalEvent = "openModal"
 
 export const routeFromChildEvent = "routeFromChild"
 
-export const GOOGLE_API_KEY = "AIzaSyCO035101Dk4VEiaerkXLR23R59WCnvFDM"
+export const GOOGLE_API_KEY = process.env.MIX_GOOGLE_API_KEY
