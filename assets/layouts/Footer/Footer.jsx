f76bd9395083efdb8878ad3df34c02b9d00dc41a
Paradoxe Ngwasi <paradoxengwasi@gmail.com>
2021-02-09 23:13:07 +0200

    fix: prefix mix var

--- assets/layouts/Footer/Footer.jsx
+++ assets/layouts/Footer/Footer.jsx
@@ -6,6 +6,7 @@ import { FiMail } from "react-icons/fi"
 import Copyright from './Copyright'
 import wmb from '/@/images/message/wmb.jpeg'
 import jesus from '/@/images/message/jesus-christ.jpeg'
+import { InertiaLink } from '@inertiajs/inertia-react'
 
 
 const FooterStyled = styled.footer`
@@ -58,11 +59,13 @@ const Footer = () => {
                         target="_blank">
                         {`https://branham.org`}
                     </a>
-                    <a
-                        className="text-sm text-decoration-underline d-block"
-                        href={route('guest.contact').toString()}>
-                        Adresse et contact
-                    </a>
+                    <div>
+                        <InertiaLink
+                            className="text-sm text-decoration-underline"
+                            href={route('guest.contact').toString()}>
+                            Adresse et contact
+                    </InertiaLink>
+                    </div>
                 </div>
                 <div className="col-lg-1" />
                 <div className="col-lg-5 mb-3">
--- assets/utils/vars.js
+++ assets/utils/vars.js
@@ -4,6 +4,6 @@ export const openModalEvent = "openModal"
 
 export const routeFromChildEvent = "routeFromChild"
 
-export const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
+export const GOOGLE_API_KEY = process.env.MIX_GOOGLE_API_KEY
 
 export const embedGoogleMap = (lat, lng, apiKey = GOOGLE_API_KEY) => `https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${lat}, ${lng}&zoom=18&language=fr`
