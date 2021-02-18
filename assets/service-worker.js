9b086034ee0aa3c5871e196efd3dc6dbdd5f4e2f
Paradoxe Ngwasi <paradoxengwasi@gmail.com>
2021-02-19 00:13:34 +0200

    [sw] pause cache file

--- assets/service-worker.js
+++ assets/service-worker.js
@@ -8,23 +8,23 @@ import { registerRoute } from 'workbox-routing';
 
 const assets = self.__WB_MANIFEST || []
 
-precacheAndRoute(assets);
+// precacheAndRoute(assets);
 
-registerRoute(
-    /\/storage\/(.+)\.(?:jpeg|jpg)/,
-    new StaleWhileRevalidate({
-        cacheName: 'images-cache',
-        plugins: [
-            new CacheableResponsePlugin({
-                statuses: [0, 200],
-            }),
-            new ExpirationPlugin({
-                maxEntries: 60,
-                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-            }),
-        ],
-    }),
-);
+// registerRoute(
+//     /\/storage\/(.+)\.(?:jpeg|jpg)/,
+//     new StaleWhileRevalidate({
+//         cacheName: 'images-cache',
+//         plugins: [
+//             new CacheableResponsePlugin({
+//                 statuses: [0, 200],
+//             }),
+//             new ExpirationPlugin({
+//                 maxEntries: 60,
+//                 maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+//             }),
+//         ],
+//     }),
+// );
 
 self.addEventListener('push', function (event) {
 
