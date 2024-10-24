110371ca5d582b6316d6c8677d678db2848e0f19
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2024-10-24 06:41:01 +0000

    Update map URL and standardize exports formatting

--- assets/utils/vars.js
+++ assets/utils/vars.js
@@ -1,10 +1,14 @@
-export const modalItemsUpdatedEvent = "modalItemsUpdated"
+export const modalItemsUpdatedEvent = "modalItemsUpdated";
 
-export const openModalEvent = "openModal"
+export const openModalEvent = "openModal";
 
-export const routeFromChildEvent = "routeFromChild"
+export const routeFromChildEvent = "routeFromChild";
 
 // @ts-ignore
-export const GOOGLE_API_KEY = process.env.NODE_ENV === "development" ? import.meta.env.VITE_GOOGLE_API_KEY : process.env.MIX_GOOGLE_API_KEY
+export const GOOGLE_API_KEY =
+    process.env.NODE_ENV === "development"
+        ? import.meta.env.VITE_GOOGLE_API_KEY
+        : process.env.MIX_GOOGLE_API_KEY;
 
-export const embedGoogleMap = (lat, lng, apiKey = GOOGLE_API_KEY) => `https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${lat}, ${lng}&zoom=18&language=fr`
+export const embedGoogleMap = (lat, lng, apiKey = GOOGLE_API_KEY) =>
+    `https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d821.3952589102082!2d28.871806153415914!3d-2.504710454811249!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMsKwMzAnMjAuMiJTIDI4wrA1MSczMy44IkU!5e1!3m2!1sen!2srw!4v1729751754692!5m2!1sen!2srw`;
